6c9e8c652c2e48242ac40e275f067a85ebe9d534
AlgoRythm-Dylan <dperunko@comcast.net>
2025-12-26 00:02:46 -0500

    small changes

--- SynTestCPP/SynTestCPP.cpp
+++ SynTestCPP/SynTestCPP.cpp
@@ -3,19 +3,27 @@
 
 #include "TestSignalGenerator.h"
 #include "Fourier.h"
+#include "ButterflyTranslator.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-constexpr float TEST_TOLERANCE = 1e-6;
-constexpr double TEST_TOLERANCE_D = 1e-9;
+constexpr float TOLERANCE = 1e-6;
+constexpr double TOLERANCE_DOUBLE_PRECISION = 1e-9;
 
 namespace SynTestCPP
 {
 	TEST_CLASS(SynTestCPP)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+
+		TEST_METHOD(Butterflies)
+		{
+			auto butterflies = Syn::computeButterflies(2048);
+			Assert::AreEqual(static_cast<size_t>(0), butterflies[0]);
+			delete butterflies;
+		}
+
+		TEST_METHOD(FFTOneFrequency)
 		{
 			TestSignalGenerator generator;
 			generator.Add(1, 3.5);
@@ -25,7 +33,8 @@ namespace SynTestCPP
 			}
 			float *outputBuffer = new float[2048];
 			transform.Transform(outputBuffer);
-			Assert::AreEqual(3.5f, outputBuffer[0], TEST_TOLERANCE);
+			Assert::AreEqual(3.5f, outputBuffer[0], TOLERANCE);
 		}
+
 	};
 }
